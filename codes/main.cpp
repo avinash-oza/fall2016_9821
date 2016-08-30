18400a6453ff82e93b0dcc45ffa6ae302d826e9e
Avinash Oza <avioza@gmail.com>
2016-08-30 15:30:54 -0400

    Add CMakefile for building and backward_subst

--- codes/main.cpp
+++ codes/main.cpp
@@ -24,19 +24,48 @@ VectorXd forward_subst(const MatrixXd & L, const VectorXd & b)
     return x;
 }
 
+VectorXd backward_subst(const MatrixXd & U, const VectorXd &b)
+{
+    int lengthOfVector = b.rows();
+    VectorXd x(lengthOfVector);
+
+    int n = lengthOfVector - 1;
+    x(n) = b(n)/(U(n, n));
+
+    for (int j = n - 1; j >= 0; j--)
+    {
+        double sum = 0;
+        for (int k = j + 1 ; k <= n; k++)
+        {
+            sum += U(j, k)*x(k);
+        }
+        x(j) = (b(j) - sum)/U(j,j);
+    }
+    return x;
+}
+
 
 int main()
 {
-    MatrixXd L(4,4);
-    VectorXd b(4);
+//    MatrixXd L(4,4);
+//    VectorXd b(4);
+//
+//    L << 100, 0,0,0,
+//            6, 106, 0, 0,
+//            8,8, 108, 0,
+//            5,5,5,105;
+//    b << 98, 104, 111,102;
+//
+//    std::cout << L << std::endl;
+//    std::cout << b << std::endl;
+//    std::cout << forward_subst(L, b) << std::endl
+    MatrixXd U(3,3);
+    VectorXd b(3);
 
-    L << 100, 0,0,0,
-            6, 106, 0, 0,
-            8,8, 108, 0,
-            5,5,5,105;
-    b << 98, 104, 111,102;
+    U << 1,2,3,0,4,5,0,0,7;
+    b << 1,2,3;
 
-    std::cout << L << std::endl;
+    std::cout << U << std::endl;
     std::cout << b << std::endl;
-    std::cout << forward_subst(L, b) << std::endl;
+    std::cout << backward_subst(U, b) << std::endl;
 }
