4e4da62907f170667fb8fda2ba546ad8ba3f3f12
Avinash Oza <avioza@gmail.com>
2016-09-12 14:40:31 -0400

    Add GaussSiedel method of solution finding

--- codes/IterativeMethods.hpp
+++ codes/IterativeMethods.hpp
@@ -9,6 +9,8 @@
 #include <iostream>
 #include <vector>
 
+#include "Solvers.hpp"
+
 using namespace Eigen;
 
 VectorXd Jacobi_Iteration(const MatrixXd &A, const VectorXd &b, const VectorXd &x0, double tol)
@@ -45,10 +47,48 @@ VectorXd Jacobi_Iteration(const MatrixXd &A, const VectorXd &b, const VectorXd &
     }
     std::cout << "Iter count: " << ic << std::endl;
 
-    #TODO: Return tuple here with iter count
+    // #TODO: Return tuple here with iter count
     return x;
 
 }
 
+VectorXd Gauss_Siedel_Iteration(const MatrixXd &A, const VectorXd &b, const VectorXd &x0, double tol)
+{
+    MatrixXd copiedA(A);
+    VectorXd x = x0;
+    VectorXd r0 = b - copiedA*x;
+
+    VectorXd residual = r0; // start at current residual
+    double stopIterResidual = tol*residual.norm();
+
+    // Input splitting
+    MatrixXd lower_A = copiedA.triangularView<Eigen::StrictlyLower>();
+    MatrixXd upper_A = copiedA.triangularView<Eigen::StrictlyUpper>();
+    MatrixXd diagonal_A = copiedA.diagonal().asDiagonal();
+
+//    std::cout << "Original:" <<std::endl << copiedA << std::endl;
+//    std::cout << "Lower:" << std::endl << lower_A  << std::endl;
+//    std::cout << "Upper" << std::endl << upper_A << std::endl;
+//    std::cout << "Diagonal" << std::endl << diagonal_A << std::endl;
+//    std::cout << lower_A + upper_A + diagonal_A - copiedA << std::endl;
+
+    // Preparation for calculation
+    MatrixXd D_inverse = diagonal_A.inverse();
+
+    VectorXd b_new = forward_subst(diagonal_A + lower_A, b); // calculate b_new
+
+    int ic = 0; // iteration count
+
+    while (residual.norm() > stopIterResidual) {
+        x = -1*forward_subst(diagonal_A + lower_A, upper_A*x) + b_new;
+        residual = b - copiedA * x;
+        ic += 1;
+    }
+    std::cout << "Iter count: " << ic << std::endl;
+
+//   #TODO: Return tuple here with iter count
+    return x;
+
+}
 
 #endif //CPPCODETEST_ITERATIVEMETHODS_HPP
