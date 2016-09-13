96392c19e730dc8cd3c97d03e88efb4e54063a65
Avinash Oza <avioza@gmail.com>
2016-09-13 17:27:49 -0400

    Adding first shot of problem 4

--- codes/Main.cpp
+++ codes/Main.cpp
@@ -19,64 +19,63 @@ MatrixXd Some(const MatrixXd& x)
 };
 
 
-int main()
-{
+int main() {
     /// Keep this line to make the decimals always print out
     std::cout << std::fixed << std::setprecision(6);
-	//// Example 1
-	//// Forward substitution for discounting rates
-	//MatrixXd m1(4, 4);
-	//m1 <<	100, 0, 0, 0,		6, 106, 0, 0,	 8, 8, 108, 0,		5, 5, 5, 105;
-	//VectorXd v1(4);
-	//v1 <<	98, 104, 111, 102;
-	//std::cout << "|-------------------------------------|\n";
-	//std::cout << "|         Forward substitution        |\n";
-	//std::cout << "|     Finding discounting factors     |\n";
-	//std::cout << "|-------------------------------------|\n";
-	//std::cout << "The bond payment matrix is\n"; std::cout << m1 << std::endl << std::endl;
-	//std::cout << "The bond prices vector is\n"; std::cout << v1 << std::endl << std::endl;
-	//std::cout << "The exponents of the discounting factors are:\n"; std::cout << forward_subst(m1, v1) << std::endl << std::endl;
-	//// Remember, the discounting factos are e^(values returned from the function).
+    //// Example 1
+    //// Forward substitution for discounting rates
+    //MatrixXd m1(4, 4);
+    //m1 <<	100, 0, 0, 0,		6, 106, 0, 0,	 8, 8, 108, 0,		5, 5, 5, 105;
+    //VectorXd v1(4);
+    //v1 <<	98, 104, 111, 102;
+    //std::cout << "|-------------------------------------|\n";
+    //std::cout << "|         Forward substitution        |\n";
+    //std::cout << "|     Finding discounting factors     |\n";
+    //std::cout << "|-------------------------------------|\n";
+    //std::cout << "The bond payment matrix is\n"; std::cout << m1 << std::endl << std::endl;
+    //std::cout << "The bond prices vector is\n"; std::cout << v1 << std::endl << std::endl;
+    //std::cout << "The exponents of the discounting factors are:\n"; std::cout << forward_subst(m1, v1) << std::endl << std::endl;
+    //// Remember, the discounting factos are e^(values returned from the function).
 
-	//// Example 2
-	//// LU decomposition without pivotting
+    //// Example 2
+    //// LU decomposition without pivotting
 //	MatrixXd m2(4, 4);
 //	m2 << 2,-1,3,0,   -4,5,-7,-2,   -2,10,-4,-7,   4,-14,8,10;
-	//std::cout << "|-------------------------------------|\n";
-	//std::cout << "|  LU Decomposition Without Pivoting  |\n";
-	//std::cout << "|           Full Matrix               |\n";
-	//std::cout << "|-------------------------------------|\n";
-	//std::cout << "The original matrix is\n"; std::cout << m2 << std::endl << std::endl;
-	//std::cout << "The L matrix is:\n" << std::get<0>(lu_no_pivoting(m2)) << std::endl << std::endl;	// L is found in position 0
-	//std::cout << "The U matrix is:\n" << std::get<1>(lu_no_pivoting(m2)) << std::endl << std::endl;	// U is found in position 1
+    //std::cout << "|-------------------------------------|\n";
+    //std::cout << "|  LU Decomposition Without Pivoting  |\n";
+    //std::cout << "|           Full Matrix               |\n";
+    //std::cout << "|-------------------------------------|\n";
+    //std::cout << "The original matrix is\n"; std::cout << m2 << std::endl << std::endl;
+    //std::cout << "The L matrix is:\n" << std::get<0>(lu_no_pivoting(m2)) << std::endl << std::endl;	// L is found in position 0
+    //std::cout << "The U matrix is:\n" << std::get<1>(lu_no_pivoting(m2)) << std::endl << std::endl;	// U is found in position 1
 
-	//// Example 3
-	//// LU decomposition with row pivoting
-	//MatrixXd m3(5, 5);
-	//m3 << 1, 2, -7, -1.5, 2, 4, 4, 0, -6, -2, -2, -1, 2, 6, 2.5, 0, -2, 2, -4, 1, 2, 1, 13, -5, 3.5;
-	//std::cout << "|-------------------------------------|\n";
-	//std::cout << "|   LU Decomposition With Pivoting    |\n";
-	//std::cout << "|           Full Matrix               |\n";
-	//std::cout << "|-------------------------------------|\n";
-	//std::cout << "The original matrix is\n"; std::cout << m3 << std::endl << std::endl;
-	//std::cout << "The L matrix is:\n" << std::get<0>(lu_pivoting(m3)) << std::endl << std::endl;	// L is found in position 0
-	//std::cout << "The U matrix is:\n" << std::get<1>(lu_pivoting(m3)) << std::endl << std::endl;	// U is found in position 1
-	//std::cout << "The U matrix is:\n" << std::get<2>(lu_pivoting(m3)) << std::endl << std::endl;	// P is found in position 2
+    //// Example 3
+    //// LU decomposition with row pivoting
+    //MatrixXd m3(5, 5);
+    //m3 << 1, 2, -7, -1.5, 2, 4, 4, 0, -6, -2, -2, -1, 2, 6, 2.5, 0, -2, 2, -4, 1, 2, 1, 13, -5, 3.5;
+    //std::cout << "|-------------------------------------|\n";
+    //std::cout << "|   LU Decomposition With Pivoting    |\n";
+    //std::cout << "|           Full Matrix               |\n";
+    //std::cout << "|-------------------------------------|\n";
+    //std::cout << "The original matrix is\n"; std::cout << m3 << std::endl << std::endl;
+    //std::cout << "The L matrix is:\n" << std::get<0>(lu_pivoting(m3)) << std::endl << std::endl;	// L is found in position 0
+    //std::cout << "The U matrix is:\n" << std::get<1>(lu_pivoting(m3)) << std::endl << std::endl;	// U is found in position 1
+    //std::cout << "The U matrix is:\n" << std::get<2>(lu_pivoting(m3)) << std::endl << std::endl;	// P is found in position 2
 
-	//// Example 4
-	//// Finding the inverse of a matrix
-	//MatrixXd m4(5, 5);
-	//m4 << 1, 2, -7, -1.5, 2,	4, 4, 0, -6, -2,	-2, -1, 2, 6, 2.5,		0, -2, 2, -4, 1,	2, 1, 13, -5, 3.5;
-	//std::cout << "|-------------------------------------|\n";
-	//std::cout << "|         Inverse of a matrix         |\n";
-	//std::cout << "|             Full Matrix             |\n";
-	//std::cout << "|-------------------------------------|\n";
-	//std::cout << "The input matrix is\n" << m4 << endl;
-	//std::cout << "The inverse of the matrix is\n" << inverse(m4) << std::endl;
-	//std::cout << "The product OF the two matrices is\n" << m4*inverse(m4) << endl;
+    //// Example 4
+    //// Finding the inverse of a matrix
+    //MatrixXd m4(5, 5);
+    //m4 << 1, 2, -7, -1.5, 2,	4, 4, 0, -6, -2,	-2, -1, 2, 6, 2.5,		0, -2, 2, -4, 1,	2, 1, 13, -5, 3.5;
+    //std::cout << "|-------------------------------------|\n";
+    //std::cout << "|         Inverse of a matrix         |\n";
+    //std::cout << "|             Full Matrix             |\n";
+    //std::cout << "|-------------------------------------|\n";
+    //std::cout << "The input matrix is\n" << m4 << endl;
+    //std::cout << "The inverse of the matrix is\n" << inverse(m4) << std::endl;
+    //std::cout << "The product OF the two matrices is\n" << m4*inverse(m4) << endl;
 
-	// Example 5
-	// Finding discounting factors using the LU decomposition
+    // Example 5
+    // Finding discounting factors using the LU decomposition
 //	MatrixXd m5(4, 4);
 //	m5 << 2, 102, 0, 0,		5, 0, 105, 0,		1.5, 1.5, 1.5, 101.5,		0, 6, 0, 106;
 //	VectorXd v5(4);
@@ -88,39 +87,127 @@ int main()
 //	std::cout << "The bond payment matrix is\n"; std::cout << m5 << std::endl << std::endl;
 //	std::cout << "The bond prices vector is\n"; std::cout << v5 << std::endl << std::endl;
 //	std::cout << showpoint << setprecision(8) << "The exponents of the discounting factors are:\n"; std::cout << linear_solve_lu_row_pivoting(m5, v5) << std::endl << std::endl;
-	// Remember, the discounting factos are e^(values returned from the function).
+    // Remember, the discounting factos are e^(values returned from the function).
 
 
     // Iterative methods test code (Problem 3 from HW3)
-    Eigen::MatrixXd A=MatrixXd::Zero(14,14);
-    Eigen::VectorXd b=VectorXd::Zero(14);
-    Eigen::VectorXd x_0=VectorXd::Zero(14);
-    double tol=0.000001;
+    Eigen::MatrixXd A = MatrixXd::Zero(14, 14);
+    Eigen::VectorXd b = VectorXd::Zero(14);
+    Eigen::VectorXd x_0 = VectorXd::Zero(14);
+    double tol = 0.000001;
 
-    for(int j=0; j<14; ++j)
-    {
-        b(j)=j*j;
-        x_0(j)=1;
-        for(int k=0; k<14; ++k)
-        {
-            if(j==k)
-                A(j,k)=2;
+    for (int j = 0; j < 14; ++j) {
+        b(j) = j * j;
+        x_0(j) = 1;
+        for (int k = 0; k < 14; ++k) {
+            if (j == k)
+                A(j, k) = 2;
 
-            else if(j==k+1)
-                A(j,k)=-1;
-            else if(j==k-1)
-                A(j,k)=-1;
+            else if (j == k + 1)
+                A(j, k) = -1;
+            else if (j == k - 1)
+                A(j, k) = -1;
             else
                 continue;
         }
     }
 
-    SORIteration jacobiIterator;
+//    SORIteration jacobiIterator;
+//
+//    cout << "ret_new:\n" << std::endl;
+//    cout << consecutive_approximation_solver(A, b, x_0, std::pow(10, -6), jacobiIterator, 1.15);
 
-    cout << "ret_new:\n" << std::endl;
-    cout << consecutive_approximation_solver(A, b, x_0, std::pow(10, -6), jacobiIterator, 1.15);
-    
+    GaussSiedelIteration iterationMethod;
+
+    //coordinates for mesh
+    double startX = 0;
+    double endX = 1;
+
+    //coordinates for y direction
+    double startY = 0;
+    double endY = 1;
+    int N = 4; // number of points to make on the x axis
+    int M = 4; // number of points on the y axis
+
+    double xStepSize = (endX - startX) / N;
+    double yStepSize = (endY - startY) / M;
+
+//    VectorXd xCoordinates(N);
+//    for (int i=0; i < N; i++)
+//    {
+//        xCoordinates(i) = startX +i*xStepSize;
+//    }
+//
+//    VectorXd yCoordinates(N-1);
+//    for (int j=0; j < M; j++)
+//    {
+//        yCoordinates(j) = startY +j*yStepSize;
+//    }
+
+    MatrixXd T(N * N, N * N);
+    T.setZero();
+    int lastMatrixValue = N - 1;
+
+    for (int i = 0; i < N * N; i++) {
+        T(i, i) = 4.0;
+    }
+
+    for (int j = 2; j < N * N; j++) {
+//        int originalLocation = j;
+        if (N % j != 0) {
+            T(j, j - 1) = -1.0;
+        }
+
+    }
+
+
+    for (int j = 1; j < N * N - 1; j++) {
+//        int originalLocation = j;
+        if (N % j != 0) {
+            T(j, j + 1) = -1.0;
+        }
+
+    }
+
+
+    for (int j = 0; j < N * N - N; j++) {
+        T(j, j + N) = -1.0;
+
+    }
+
+    for (int j = N; j < N * N - 1; j++) {
+        T(j, j - N) = -1.0;
+
+    }
+//    for (int i=0; i < N*N-1; i++)
+//    {
+//        if (N%i != 0) {
+//            T(i,i+1) = -1.0;
+//        }
+//
+//    }
+
+
+    std::cout << T << std::endl;
+    std::cout << cholesky(T) << std::endl;
+
+
+
+
+//    cout << "ret_new:\n" << std::endl;
+//    cout << consecutive_approximation_solver(A, b, x_0, std::pow(10, -6), iterationMethod, 1.15);
+
 	return 0;
+}
+
+
+double hw3FiniteDifferenceMethod(double x, double y)
+{
+    double temp1 = (x*x + y*y -2)* (std::sin(x)*std::sin(y));
+    double temp2 = 2.0*x*std::cos(x)*std::sin(y);
+    double temp3 = 2.0*y*std::sin(x)*std::cos(y);
+
+    return temp1 - temp2 - temp3;
 }
 
 void verifyCholeskyDecomposition()
