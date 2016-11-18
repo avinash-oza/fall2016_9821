5d0261312f65d995440d2c0c6fae5c6dd3bc5209
avi <avioza@gmail.com>
2016-11-18 00:08:37 -0500

    Remove code which is not used anymore

--- codes/Main.cpp
+++ codes/Main.cpp
@@ -8,7 +8,6 @@
 #include "StoppingCriterionSolvers.hpp"
 #include "Solvers.hpp"
 #include "Examples.hpp"
-#include "PDESolverFunctions.hpp"
 #include "OptionPricers.hpp"
 #include "BlackScholes.hpp"
 //#include "Options.hpp"
@@ -22,10 +21,7 @@ using namespace std;
 
 
 //defined at end
-VectorXd generateBVector(int N, VectorXd xMesh, VectorXd yMesh);
 MatrixXd generateHW3Matrix(int N);
-double u(double x, double y);
-VectorXd calculateuExactVector(int N, const VectorXd &xCoordinates, const VectorXd &yCoordinates);
 void runOneIteration(int N, double w);
 void printCSVMatrix(std::string stringToPrint, const MatrixXd& myMatrix);
 void Question3();
@@ -43,44 +39,6 @@ int main() {
 //    {
 //        calculateTrinomialTreesForN(i);
 //    }
-
-    // Iterative methods test code (Problem 3 from HW3)
-//    Eigen::MatrixXd A = MatrixXd::Zero(14, 14);
-//    Eigen::VectorXd b = VectorXd::Zero(14);
-//    Eigen::VectorXd x_0 = VectorXd::Zero(14);
-//    double tol = 0.000001;
-//
-//    for (int j = 0; j < 14; ++j) {
-//        b(j) = j * j;
-//        x_0(j) = 1;
-//        for (int k = 0; k < 14; ++k) {
-//            if (j == k)
-//                A(j, k) = 2;
-//
-//            else if (j == k + 1)
-//                A(j, k) = -1;
-//            else if (j == k - 1)
-//                A(j, k) = -1;
-//            else
-//                continue;
-//        }
-//    }
-
-//    SORIteration jacobiIterator;
-//
-//    cout << "ret_new:\n" << std::endl;
-//    cout << consecutive_approximation_solver(A, b, x_0, std::pow(10, -6), jacobiIterator, 1.15);
-//
-//    vector<int> NtoRunFor= {2};
-//    for (double w = 1.02; w < 1.04; w+=0.02) {
-//        cout << w << "|";
-//        for (auto it = NtoRunFor.begin(); it != NtoRunFor.end(); it++) {
-//            runOneIteration(*it, w);
-//    std::cout << ",";
-//        }
-//        cout << endl;
-//    }
-
 
     return 0;
 }
@@ -114,80 +72,14 @@ void hw8()
         cout << M << "," << N  << ",";
         PDESolver solver(gLeft, gRight, f, 0, tauFinal, xLeft, xRight, M, N);
 //        MatrixXd fEulerResult = solver.forwardEuler();
-//        MatrixXd bEulerResult = solver.backwardEuler(SOR, tol, omega);
-        MatrixXd bEulerResult = solver.CrankNicolson(SOR, tol, omega);
+        MatrixXd bEulerResult = solver.backwardEuler(SOR, tol, omega);
+//        MatrixXd bEulerResult = solver.CrankNicolson(SOR, tol, omega);
 //        std::cout << fEulerResult << std::endl;
         std::cout << solver.MaxPointwiseApproximationError(bEulerResult, uExact1)
         << "," <<  solver.RootMeanSquaredError(bEulerResult, uExact1) << std::endl;
     }
 
 
-}
-
-
-void runOneIteration(int N, double w) {
-    //Start building the mesh
-    double startX = 0;
-    double endX = 1;
-
-    //coordinates for y direction
-    double startY = 0;
-    double endY = 1;
-
-    // h for x and y
-    double xStepSize, yStepSize;
-
-    int M = N;
-
-    VectorXd xCoordinates, yCoordinates;
-
-    std::tie(xCoordinates, yCoordinates) = generateMeshPoints(startX, endX, startY, endY, N, M);
-    std::tie(xStepSize, yStepSize) = getStepSizes(startX, endX, startY, endY, M, N);
-
-    // Finish building the mesh
-
-    // Construct the interation method and initalize the initial vector
-    SORIteration iterationMethod;
-    VectorXd x0(N*N);
-    x0.setZero();
-
-    // generate the matrix T
-    MatrixXd T = generateHW3Matrix(N);
-    VectorXd b = get_b(N, xCoordinates, yCoordinates, xStepSize, yStepSize);
-
-    // calculate the exact values we expect
-    VectorXd uExact = calculateuExactVector(N, xCoordinates, yCoordinates);
-
-    time_t startTime(time(NULL));
-
-    double ic;
-    VectorXd iterativeMethodResult;
-    std::tie(iterativeMethodResult, ic) = residual_based_solver(T, b, x0, std::pow(10, -6), iterationMethod, w);
-
-    time_t endTime(time(NULL));
-
-    printResidualError("Error for iterative result", T, iterativeMethodResult, b);
-    cout << maxApproximationError(iterativeMethodResult, uExact);
-//    std::cout << "The time elapsed was " << endTime - startTime << " seconds" << std::endl;
-
-}
-
-VectorXd calculateuExactVector(int N, const VectorXd &xCoordinates, const VectorXd &yCoordinates) {
-    // calculates the given function u over the x and y coordinates given
-    VectorXd uExact(N * N);
-    int vectorLocation = 0;
-
-    for (int yIndex = 0 ; yIndex < N; yIndex++)
-    {
-        for (int xIndex = 0; xIndex < N; xIndex++)
-        {
-            // MAKE SURE TO CHANGE THIS FUNCTION IF NEEDED
-            uExact(vectorLocation) = u(xCoordinates(xIndex), yCoordinates(yIndex));
-//            std::cout << "Mesh point is (" << timeCoordinates(xIndex) << ", " << xCoordinates(yIndex) << ")" << std::endl;
-            vectorLocation++;
-        }
-    }
-    return uExact;
 }
 
 MatrixXd generateHW3Matrix(int N) {
