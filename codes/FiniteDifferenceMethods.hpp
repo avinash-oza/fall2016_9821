993b8287d647488bd4895189dd2ba15d17761dc6
avi <avioza@gmail.com>
2016-11-23 12:25:53 -0500

    Initial code for option pricing

--- codes/FiniteDifferenceMethods.hpp
+++ codes/FiniteDifferenceMethods.hpp
@@ -7,6 +7,8 @@
 #include <Eigen/Dense>
 #include "PDEMesh.hpp"
 
+
+using namespace Eigen;
 enum LinearSolveMethod {LU, SOR}; // define enum for linear solve
 
 class uFunction
@@ -26,6 +28,28 @@ class hw8f : public uFunction
 
 };
 
+class hw8fOption : public uFunction
+{
+	public:
+		double sigma;
+		double S0;
+		double q;
+		double K;
+		double T;
+		double r;
+		double a;
+		hw8fOption(double _sigma, double _S0, double _q, double _K, double _T, double _r):
+			sigma(_sigma), S0(_S0), q(_q), K(_K), T(_T), r(_r)
+		{
+			a = (r-q)/(sigma*sigma) - 0.5;
+		}
+		double evaluate(double x, double t)
+		{
+			return K*exp(a*x)*std::max(1-exp(x),0.0);
+		}
+
+};
+
 class hw8gLeft : public uFunction
 {
     public:
@@ -34,6 +58,29 @@ class hw8gLeft : public uFunction
         return std::exp(t - 2.0);
     }
 
+};
+
+class hw8gLeftOption : public uFunction
+{
+	public:
+		double sigma;
+		double S0;
+		double q;
+		double K;
+		double T;
+		double r;
+		double a;
+		double b;
+		hw8gLeftOption(double _sigma, double _S0, double _q, double _K, double _T, double _r):
+			sigma(_sigma), S0(_S0), q(_q), K(_K), T(_T), r(_r)
+		{
+			a = (r-q)/(sigma*sigma) - 0.5;
+			b = pow((r-q)/(sigma*sigma) + 0.5, 2) + 2*q/(sigma*sigma);
+		}
+		double evaluate(double x, double t)
+		{
+			return K*exp(a*x + b*t)*(exp(-(2*r*t)/(sigma*sigma)) - exp(x - 2*q*t/(sigma*sigma)));
+		}
 };
 
 class uExact : public uFunction
@@ -56,6 +103,14 @@ class hw8gRight : public uFunction
 
 };
 
+class hw8gRightOption : public uFunction
+{
+	public:
+    double evaluate(double x, double t)
+    {
+        return 0.0;
+    }
+};
 //M time intervals, N x intervals
 
 class PDESolver
@@ -67,7 +122,15 @@ public:
         mesh = Mesh(0, tFinal, xLeft, xRight, M, N);
     };
 
+	PDESolver(uFunction &gLeftFunc, uFunction &gRightFunc, uFunction &f, double t0, double tFinal, double xLeft, double xRight, int M, double alphatemp) :
+            _gLeftFunc(gLeftFunc), _gRightFunc(gRightFunc),
+            _f(f), _t0(t0), _tFinal(tFinal), _xLeft(xLeft), _xRight(xRight), M(M){
+		N = std::floor((xRight-xLeft)/sqrt(tFinal/(alphatemp*M)));
+        mesh = Mesh(0, tFinal, xLeft, xRight, M, N);
+    };
 
+	double getM() const {return M;};
+	double getN() const {return N;};
     Eigen::MatrixXd forwardEuler()
     {
         Eigen::MatrixXd valuesAtNodes = Eigen::MatrixXd::Zero(M + 1, N + 1);
@@ -376,13 +439,14 @@ private:
     uFunction & _gLeftFunc;
     uFunction & _gRightFunc;
     uFunction & _f;
-    Mesh mesh;
     double _t0;
     double _tFinal;
     double _xLeft;
     double _xRight;
     long M;
     long N;
+public:
+	Mesh mesh;
 
 
 };
--- codes/Main.cpp
+++ codes/Main.cpp
@@ -1,5 +1,7 @@
 #include <tuple>
 #include <iomanip>
+#include<iostream>
+#include<fstream>
 #include <cmath>
 #include <time.h>
 #include <vector>
@@ -7,26 +9,22 @@
 #include "IterativeMethods.hpp"
 #include "StoppingCriterionSolvers.hpp"
 #include "Solvers.hpp"
-#include "Examples.hpp"
 #include "OptionPricers.hpp"
 #include "BlackScholes.hpp"
 //#include "Options.hpp"
-#include "RandomNumberGenerator.hpp"
-#include "MonteCarlo.hpp"
 #include "FiniteDifferenceMethods.hpp"
 
 
 using namespace Eigen;
 using namespace std;
 
-
+const static Eigen::IOFormat CSVFormat(Eigen::FullPrecision, 0, ",", "\n");
 //defined at end
 MatrixXd generateHW3Matrix(int N);
 void runOneIteration(int N, double w);
 void printCSVMatrix(std::string stringToPrint, const MatrixXd& myMatrix);
 void Question3();
 void hw8();
-
 int main() {
     /// Keep this line to make the decimals always print out
     std::cout << std::fixed << std::setprecision(9);
@@ -53,17 +51,34 @@ void hw8()
     double xLeft = -2.0;
     double xRight = 2.0;
     double tauFinal = 1.0;
-    long M = 2;
-    long N = 8;
+    //long M = 2;
+    //long N = 8;
+    long M = 1;
     cout << setprecision(16);
     double tol = std::pow(10, -6);
     double omega = 1.2;
+
+    double sigma = 0.35;
+    double S0 = 41;
+    double q = 0.02;
+    double K = 40;
+    double T = 0.75;
+    double r = 0.04;
+    double alphatemp = 0.45;
+    double a = (r-q)/(sigma*sigma) - 0.5;
+    double b = pow((r-q)/(sigma*sigma) + 0.5, 2) + 2*q/(sigma*sigma);
+    double xLeftOption = log(S0/K) + (r-q-sigma*sigma/2) * T - 3*sigma*sqrt(T);
+    double xRightOption = log(S0/K) + (r-q-sigma*sigma/2) * T + 3*sigma*sqrt(T);
+    double tauFinalOption = T*sigma*sigma/2;
+    hw8fOption fOption(sigma, S0, q, K, T, r);
+    hw8gLeftOption gLeftOption(sigma, S0, q, K, T, r);
+    hw8gRightOption gRightOption;
 
 //    PDESolver solver(gLeft, gRight, f, 0, tauFinal, xLeft, xRight, M, N);
 //    MatrixXd fEulerResult = solver.CrankNicolson(LU, tol, omega);
 //    MatrixXd bEulerResult = solver.CrankNicolson(LU, tol, omega);
 //    printCSVMatrix("Print" ,bEulerResult);
-
+    /*
     for (int i = 0; i < 4; ++i)
     {
         M *= 4;
@@ -78,84 +93,58 @@ void hw8()
         std::cout << solver.MaxPointwiseApproximationError(bEulerResult, uExact1)
         << "," <<  solver.RootMeanSquaredError(bEulerResult, uExact1) << std::endl;
     }
-
-
-}
-
-MatrixXd generateHW3Matrix(int N) {
-    int NSquared = N*N;
-    MatrixXd T(NSquared, NSquared);
-    T.setZero();
-
-    for (int i = 0; i < N * N; i++) {
-        T(i, i) = 4.0;
-    }
-
-    for (int j = 1; j < N * N; j++) {
-        int valueToCheck = (j + 1)-1;
-        if (valueToCheck % N != 0) {
-            T(j, j - 1) = -1.0;
-        }
-    }
-
-    for (int j = 0; j < N * N - 1; j++) {
-        int valueToCheck = j + 1;
-        if (valueToCheck % N != 0) {
-            //assign based on original index, check based on 1 indexed
-            T(j, j + 1) = -1.0;
-        }
-    }
-
-    for (int j = 0; j < N * N - N; j++) {
-        T(j, j + N) = -1.0;
-    }
-
-    for (int j = N; j < N * N; j++) {
-        T(j, j - N) = -1.0;
-    }
-    return T;
-
-}
-
-void Question3()
-{
-    // Calculating the option price.
-    double spot, strike, interest, vol, maturity, div;
+    */
     BlackScholesOption option;
     double price;
     spot = 50; strike = 55; interest = 0.04; vol = 0.3; maturity = 0.5; div = 0;
     price = option.putPrice(spot, strike, maturity, div, interest, 0, vol);
 
-    // Creating the vector containing the value for the number of random variables.
-//    int N_vector_size = 10;
-    int N_vector_size = 1;
-    Eigen::VectorXi N_vector = Eigen::VectorXi::Zero(N_vector_size);
-//    N_vector << 1, 2, 4, 8, 16, 32, 64, 128, 256, 512;
-    N_vector << 1;
-    N_vector *= 10000;
+        //RMS Error
+        VectorXd Vappro = VectorXd::Zero(N+1);
+        VectorXd S = VectorXd::Zero(N+1);
+        double Nrms = 0;
+        double sum = 0;
+        for(int j = 0; j < N+1; ++j)
+        {
+            S(j) = K*exp(solver.mesh.getX(j));
+            Vappro(j) = EulerResultOption(M,j)*exp(-a*solver.mesh.getX(j)-b*tauFinalOption);
+            double price = option.putPrice(S(j), K, T, q, r, 0, sigma);
+            if(price > 0.00001*S0)
+            {
+                ++Nrms;
+                sum = sum + pow(Vappro(j) - price , 2)/pow(price,2);
+            }
+        }
+        result(count,4)=sqrt(sum/Nrms);
+        if(count>0)
+        {
+            result(count,1) = result(count, 0)/result(count-1,0);
+            result(count,3) = result(count, 2)/result(count-1,2);
+            result(count,5) = result(count, 4)/result(count-1,4);
+        }
 
+        // Greeks
+        double delta = (Vappro(i+1) - Vappro(i))/(S(i+1) - S(i));
+        result(count,6)=delta;
+        //cout<<delta;
+        double delta2 = (Vappro(i+2) - Vappro(i+1))/(S(i+2) - S(i+1));
+        double delta0 = (Vappro(i) - Vappro(i-1))/(S(i) - S(i-1));
+        double gamma = (delta2-delta0)/((S(i+2)+S(i+1)-S(i)-S(i-1))/2);
+        result(count,7)=gamma;
+        //cout<<gamma;
+        double dT = 2*(solver.mesh.getT(M) - solver.mesh.getT(M-1))/(sigma*sigma);
+        double V1dT = EulerResultOption(M-1,i)*exp(-a*solver.mesh.getX(i)-b*solver.mesh.getT(M-1));
+        double V2dT = EulerResultOption(M-1,i+1)*exp(-a*solver.mesh.getX(i+1)-b*solver.mesh.getT(M-1));
+        double Vt = ((S2-S0)*V1dT + (S0-S1)*V2dT)/(S2-S1);
+        double theta = (Vappro1 - Vt)/dT;
+        result(count,8)=theta;
+        //cout<<theta;
+    }
+    cout<<'1';
+    std::ofstream myfile("output2.csv");
+    myfile<<(result).format(CSVFormat)<<std::endl;
+    myfile.close();
 
-    // Part a: Iverse Transform Method
-    InverseTransformMethod inverseTransformMethod;
-    LinearCongruentialGenerator uniformMethod;
-    MonteCarloMethod monteCarloPricer;
-
-    monteCarloPricer.runMonteCarloForPaths(spot, strike, interest, vol, div, maturity, N_vector, inverseTransformMethod, uniformMethod, price);
-//    double vol2 = 0.2, spot2 = 30;
-//    spot = 25; strike = 50; interest = 0.05; vol = 0.3; maturity = 0.5; div = 0;
-//
-//    BasketOptionMonteCarloMethod basketOptionMonteCarloMethod(0.35);
-//    basketOptionMonteCarloMethod.runMonteCarloForPaths(spot, spot2, strike, interest, vol, vol2, div, maturity, N_vector, inverseTransformMethod, uniformMethod, price);
-
-
-//     Part b: Acceptance Rejection Method
-//    AcceptanceRejectionMethod acceptanceRejectionMethod;
-//    runMonteCarloForPaths(spot, strike, interest, vol, div, maturity, N_vector, acceptanceRejectionMethod, uniformMethod, price);
-
-
-//    std::cout << std::endl << std::endl;
-//     Part c: Box  Muller Method
-//    BoxMullerMethod boxMullerMethod;
-//    runMonteCarloForPaths(spot, strike, interest, vol, div, maturity, N_vector, boxMullerMethod, uniformMethod, price);
-//    std::cout << std::endl << std::endl;
 }
+
+
