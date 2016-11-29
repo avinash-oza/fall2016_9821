6d9c1ccc859da5370fc049168c838f239345b020
avi <avioza@gmail.com>
2016-11-29 11:35:38 -0500

    First round of commenting and deleting extra code

--- codes/uFunctions.hpp
+++ codes/uFunctions.hpp
@@ -5,27 +5,50 @@
 #ifndef CPPCODETEST_UFUNCTIONS_HPP
 #define CPPCODETEST_UFUNCTIONS_HPP
 
+#include <Eigen/Dense>
+
+/**
+ * Base function for defining boundary conditions. All boundary functions should inherit from here somehow.
+ */
 class uFunction
 {
     public:
+    /**
+     * Main entry point for uFunction. This will be the method called for calculation
+     * @param x
+     * @param t
+     * @return
+     */
         virtual double evaluate(double x, double t) = 0;
 
 };
-
+/**
+ * uOptionFunction provides an interface for defining boundary conditions driven by option inputs.
+ */
 class uOptionFunction : public uFunction
+
 {
-    // to make it easier to implement option functionality
 public:
     uOptionFunction(double _sigma, double _S0, double _q, double _K, double _T, double _r):
             sigma(_sigma), S0(_S0), q(_q), K(_K), T(_T), r(_r)
     {
-        a = (r-q)/(sigma*sigma) - 0.5;
-        b = pow((r-q)/(sigma*sigma) + 0.5, 2) + 2*q/(sigma*sigma);
+        seta();
+        setb();
     }
 
     double evaluate(double x, double t)
     {
         return K * exp(a * x) * std::max(1 - exp(x), 0.0);
+    }
+
+    void seta()
+    {
+        a = (r-q)/(sigma*sigma) - 0.5;
+    }
+
+    void setb()
+    {
+        b = pow((r-q)/(sigma*sigma) + 0.5, 2) + 2*q/(sigma*sigma);
     }
 
     double sigma;
@@ -37,6 +60,9 @@ public:
     double a;
     double b;
 };
+
+
+/////////////////////////////////////// IMPLEMENTATIONS//////////////////////////////////////////
 
 class hw8f : public uFunction
 {
@@ -73,12 +99,7 @@ class hw8gLeft : public uFunction
 class hw8gLeftOption : public uOptionFunction
 {
 	public:
-		hw8gLeftOption(double _sigma, double _S0, double _q, double _K, double _T, double _r):
-			uOptionFunction(_sigma, _S0, _q, _K, _T, _r)
-		{
-			a = (r-q)/(sigma*sigma) - 0.5;
-			b = pow((r-q)/(sigma*sigma) + 0.5, 2) + 2*q/(sigma*sigma);
-		}
+        using uOptionFunction::uOptionFunction;
 		double evaluate(double x, double t)
 		{
 			return K * exp(a * x + b * t) * (exp(-(2 * r * t) / (sigma * sigma)) - exp(x - 2 * q * t / (sigma * sigma)));
@@ -125,16 +146,5 @@ public:
     }
 };
 
-//class fAmericanBoundaryFunc : public uOptionFunction
-//{
-//public:
-//    using uOptionFunction::uOptionFunction;
-//    double evaluate(double x, double t)
-//    {
-//        return K*std::exp(a*x + b*t) * std::max(1.0 - std::exp(x), 0.0);
-//    }
-//};
-
-#include <Eigen/Dense>
 
 #endif //CPPCODETEST_UFUNCTIONS_HPP
